932e5a98664b75b787aec5aa070dbfad5f562425
chbmb <chbmb@linuxserver.io>
2018-09-13 21:20:57 +0100

    Update kernel-compile-module.sh

--- build_scripts/kernel-compile-module.sh
+++ build_scripts/kernel-compile-module.sh
@@ -26,18 +26,17 @@ umount -l /lib/firmware/
 rm -rf  /lib/firmware
 mv -f  /tmp/firmware /lib
 
-##Download and Install Kernel
+##Download and Install Kernel 
 [[ $(uname -r) =~ ([0-9.]*) ]] &&  KERNEL=${BASH_REMATCH[1]} || return 1
   LINK="https://www.kernel.org/pub/linux/kernel/v4.x/linux-${KERNEL}.tar.xz"
   rm -rf $D/kernel; mkdir $D/kernel
   [[ ! -f $D/linux-${KERNEL}.tar.xz ]] && wget $LINK -O $D/linux-${KERNEL}.tar.xz
-
   tar -C $D/kernel --strip-components=1 -Jxf $D/linux-${KERNEL}.tar.xz
   rsync -av /usr/src/linux-$(uname -r)/ $D/kernel/
   cd $D/kernel
   for p in $(find . -type f -iname "*.patch"); do patch -N -p 1 < $p
   done
-  make oldconfig
+  make menuconfig
 
 ##Compile Kernel
 cd $D/kernel
@@ -47,35 +46,19 @@ make -j $(grep -c ^processor /proc/cpuinfo)
 cd $D/kernel
 make all modules_install install
 
-##Install RocketRaid
-mkdir -p /usr/src/drivers/highpoint
-cd /usr/src/drivers/highpoint
-wget http://www.highpoint-tech.com/BIOS_Driver/R750/Linux/R750_Linux_Src_v$ROCKET.tar.gz
-tar xf R750_Linux_Src_v$ROCKET.tar.gz
-echo "Build out of tree driver: RocketRaid r750"
-( cd /usr/src/drivers/highpoint
-  rm -rf r750-linux-src-v$ROCKETSHORT
-  ./r750-linux-src-v$ROCKET.bin --keep --noexec --target r750-linux-src-v$ROCKETSHORT )
-( cd /usr/src/drivers/highpoint/r750-linux-src-v$ROCKETSHORT/product/r750/linux/
-  make KERNELDIR=$D/kernel
-  xz -f r750.ko
-  install -m 644 -o root -g root r750.ko.xz -D -t /lib/modules/$(uname -r)/kernel/drivers/scsi/ )
-
-##Install Intel 10GB drivers
-mkdir -p /usr/src/drivers/intel
-cd /usr/src/drivers/intel
-wget https://downloadmirror.intel.com/14687/eng/ixgbe-$IXGBE.tar.gz
-wget https://downloadmirror.intel.com/18700/eng/ixgbevf-$IXGBEVF.tar.gz
-tar xf ixgbe-*.tar.gz
-tar xf ixgbevf-*.tar.gz
-echo "Build out of tree driver: Intel 10Gbit Ethernet"
-( cd /usr/src/drivers/intel
-  cd ixgbe-*/src
-  BUILD_KERNEL=$KERNEL_VERSION make install )
-
-( cd /usr/src/drivers/intel
-  cd ixgbevf-*/src
-  BUILD_KERNEL=$KERNEL_VERSION make install )
+##Install RocketRaid - remove at the moment until LT give driver source code location.
+#mkdir -p /usr/src/drivers/highpoint
+#cd /usr/src/drivers/highpoint
+#wget http://www.highpoint-tech.com/BIOS_Driver/R750/Linux/R750_Linux_Src_v$ROCKET.tar.gz
+#tar xf R750_Linux_Src_v$ROCKET.tar.gz
+#echo "Build out of tree driver: RocketRaid r750"
+#( cd /usr/src/drivers/highpoint
+#  rm -rf r750-linux-src-v$ROCKETSHORT
+#  ./r750-linux-src-v$ROCKET.bin --keep --noexec --target r750-linux-src-v$ROCKETSHORT )
+#( cd /usr/src/drivers/highpoint/r750-linux-src-v$ROCKETSHORT/product/r750/linux/
+#  make KERNELDIR=$D/kernel
+#  xz -f r750.ko
+#  install -m 644 -o root -g root r750.ko.xz -D -t /lib/modules/$(uname -r)/kernel/drivers/scsi/ )
 
 ##Download Unraid
 cd $D
@@ -122,6 +105,9 @@ cd $D/$VERSION/stock/
 md5sum bzimage-new > bzimage-new.md5
 md5sum bzmodules-new > bzmodules-new.md5
 md5sum bzfirmware-new > bzfirmware-new.md5
+
+##Return to original directory
+cd $D
 
 ##Return to original directory
 cd $D
